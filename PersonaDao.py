f4ef081bc0347173cee18a6cdcccc66fcc6488a7
neon48 <99106583+neon48@users.noreply.github.com>
2022-03-26 10:42:53 -0500

    Actualizacion 26_03

---
+++ PersonaDao.py
@@ -0,0 +1,39 @@
+from Persona import Persona
+from loger_base import log
+from Conexion import Conexion
+
+
+class PersonaDao:
+    '''
+    DAO (Data Access Object)
+    CRUD (Create - read-update-delete)
+    '''
+
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s,%s,%s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre = %s, apellido=%s, email=%s WHERE id_persona= %s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona = %s'
+
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerCursor() as cursor:
+            cursor.execute(cls._SELECCIONAR)
+            registros = cursor.fetchall()
+            personas = []
+            for registro in registros:
+                persona = Persona(registro[0],registro[1],registro[2],registro[3])
+                personas.append(persona)
+            return personas
+
+    @classmethod
+    def insertar(cls):
+        with Conexion.obtenerConexion() as conexion:
+            with Conexion.obtenerCursor() as cursor:
+                valores = (Persona.nombre, Persona.apellido, Persona.email)
+                cursor.execute(cls._INSERTAR,valores)
+                log.debug(f'Persona Insertada: {Persona}')
+
+
+persona1 = Persona(nombre='Carlos', apellido='Aguilar', email='asda@asda.com')
+personas_insertadas = PersonaDao.insertar(persona1)
+log.debug(f'personas insertadas: {personas_insertadas}')
